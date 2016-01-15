2a25bee4306ddb644d61a7213e6e7dee16149048
Rob Johansen <robert_johans22@hotmail.com>
2016-01-14 20:39:43 -0700

    Commit a working implemention of the emailExists() database service function!

--- web/services/db-service.js
+++ web/services/db-service.js
@@ -11,8 +11,29 @@ module.exports = {
         return this;
     },
     
-    emailExists: function(email) {
-        // TODO and WYLO 2 .... Use your new email_exists() stored function!
+    emailExists: function(email, callback) {
+        pg.connect(this.connectionString, function(err, client, done) {
+            if (err) {
+                callback('Error getting client connection: ' + err);
+                return;
+            }
+            client.query({
+                name: 'email_exists_query',
+                text: 'SELECT email_exists($1)',
+                values: [email]
+            },
+            function (err, result) {
+                if (err) {
+                    callback('Error performing email_exists() query: ' + err);
+                    return;
+                }
+                if (result && result.rows && result.rows[0] && result.rows[0].email_exists) {
+                    callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+            });
+        });
     },
     
     end: function() {
--- web/test/sql/db-service-tests.js
+++ web/test/sql/db-service-tests.js
@@ -10,7 +10,6 @@ var expect   = require('chai').expect;
 describe('The db service', function() {
     
     before(function() {
-        console.log('booger');
         this.timeout(0);
         this.dbService = require('../../services/db-service').init(dbConfig);
     });
@@ -18,55 +17,64 @@ describe('The db service', function() {
     describe('emailExists()', function() {
         
         before(function(beforeDone) {
-            console.log('1');
-            pg.connect(this.dbService.connectionString, function(err, client, done) {
-                if (err) {
-                    console.log('Error connecting to database:', err);
-                    return;
-                } else {
-                    console.log('no error here...');
-                }
-                client.query("INSERT INTO users VALUES('11111111-2222-3333-4444-555555555555', 'foo@bar.com', 'password')", function(err, result) {
-                    if (err) {
-                        console.log('Error inserting test user:', err);
-                        return;
-                    }
-                    console.log('result is:', result);
-                    // TODO and WYLO .... This is what the result object looked like:
-                    //                    { command: 'INSERT', rowCount: 1, rows: [], fields: null }
-                    //
-                    //                    Remember that the polaris user still has no permissions!
-                    done();
-                    beforeDone();
-                });
-            });
-            console.log('2');
-        });
-        
-        it('should return true when the email exists', function(done) {
-            console.log('run this!');
-            expect(true).to.be.true;
-            done();
-        });
-        
-        /*
-        after(function() {
             pg.connect(this.dbService.connectionString, function(err, client, done) {
                 if (err) {
                     console.error('Error connecting to database:', err);
                     return;
                 }
-                client.query('DELETE FROM users WHERE uuid = "11111111-2222-3333-4444-555555555555"', function(err, result) {
+                client.query("INSERT INTO users VALUES('11111111-2222-3333-4444-555555555555', 'foo@bar.com', 'password')", function(err, result) {
+                    if (err) {
+                        console.error('Error inserting test user:', err);
+                        return;
+                    }
+                    if (!result || !result.rowCount || result.rowCount != 1) {
+                        console.error('Unknown error inserting test user.');
+                    }
+                    done();
+                    beforeDone();
+                });
+            });
+        });
+        
+        it('should return true when the email exists', function(done) {
+            this.dbService.emailExists('foo@bar.com', function(err, result) {
+                if (err) {
+                    console.error(err);
+                }
+                expect(result).to.be.true;
+                done();
+            });
+        });
+
+        it('should return false when the email does not exist', function(done) {
+            this.dbService.emailExists('bar@foo.com', function(err, result) {
+                if (err) {
+                    console.error(err);
+                }
+                expect(result).not.to.be.true;
+                done();
+            });
+        });
+        
+        after(function(afterDone) {
+            pg.connect(this.dbService.connectionString, function(err, client, done) {
+                if (err) {
+                    console.error('Error connecting to database:', err);
+                    return;
+                }
+                client.query("DELETE FROM users WHERE uuid = '11111111-2222-3333-4444-555555555555'", function(err, result) {
                     if (err) {
                         console.error('Error deleting test user:', err);
                         return;
                     }
-                    console.log('result is:', result);
+                    if (!result || !result.rowCount || result.rowCount != 1) {
+                        console.error('Unknown error deleting test user.');
+                    }
                     done();
+                    afterDone();
                 });
             });
         });
-        */
         
     });
     
