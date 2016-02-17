2864877b35394c3888b670f1f3ccc113b6ca464c
Rob Johansen <robert_johans22@hotmail.com>
2016-02-16 21:42:13 -0700

    Commit the beginnings of mapping the game object.

---
+++ app/flow/game.js
@@ -0,0 +1,18 @@
+{
+    round: 1,
+    turn: 1,
+    player1: {
+        uuid: '11111111-2222-3333-4444-555555555555',
+        score: 0,
+        cards: [2]
+    },
+    player2: {
+        uuid: '21111111-2222-3333-4444-555555555555',
+        score: 0,
+        cards: [4]
+    },
+    wordPool: [
+        'avocado', 'intelligent', 'exactly'
+    ],
+    board: // TODO and WYLO .... JSON doesn't support sparse arrays
+}
