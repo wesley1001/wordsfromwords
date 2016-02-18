d45e02202728bb0c9d9ef7f76c6debd256496abf
Rob Johansen <robert_johans22@hotmail.com>
2016-02-17 22:11:39 -0700

    Commit a good-enough-for-now representation of a game object.

--- app/flow/game.js
+++ app/flow/game.js
@@ -1,18 +1,19 @@
 {
-    round: 1,
+    round: 3,
     turn: 1,
     player1: {
-        uuid: '11111111-2222-3333-4444-555555555555',
-        score: 0,
-        cards: [2]
+        score: 78,
+        cards: [2, 3]
     },
     player2: {
-        uuid: '21111111-2222-3333-4444-555555555555',
-        score: 0,
+        score: 84,
         cards: [4]
     },
     wordPool: [
         'avocado', 'intelligent', 'exactly'
     ],
-    board: // TODO and WYLO .... JSON doesn't support sparse arrays
+    board: [
+        ['A', 'N', 'D' ... ['']],
+        ['T', 'O', ''  ... ['']]
+    ]
 }
